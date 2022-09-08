e4b7d863867d549c8cbbc33ab217329ded2df5a7
_水_ <lightcity123@gmail.com>
2022-09-08 12:20:28 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Bio logo" src="./assets/testlogo.png" width="250" height="250" />
+  <HelloWorld msg="Updates Test" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="Bio">
+    <h1>{{ msg }}</h1>
+    <p>
+      Hello! This is my first implentation of my website. There might be bugs
+      and strange formatting until a stable revision!
+    </p>
+    <h3>Relevant Contact Points</h3>
+    <ul>
+      <li>
+        <a href="https://github.com/mizumisuzu" target="_blank" rel="noopener"
+          >Github</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/mizu_misuzuru"
+          target="_blank"
+          rel="noopener"
+          >Twitter</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
