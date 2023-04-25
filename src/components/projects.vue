2f7da94d83f933064c458f864daf97c2ca7040a5
_水_ <lightcity123@gmail.com>
2023-04-24 23:11:35 -0500

    rev 0.03

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,9 +1,72 @@
-<script>
-export default {
-  name: "projects",
-};
-</script>
 <template>
-  <h3 style="color: #b9bbbe">blog</h3>
-  <router-view></router-view>
+  <div class="container">
+    <nav class="nav">
+      <ul>
+        <li>
+          <!-- Page routing!-->
+          <router-link to="/" class="nav-item">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/projects" class="nav-item">Projects</router-link>
+        </li>
+        <li>
+          <router-link to="/blog" class="nav-item">Blog</router-link>
+        </li>
+        <li>
+          <router-link to="/contact" class="nav-item">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <h3 style="color: #b9bbbe">🚧Currently being Developed🚧</h3>
+    <h4 style="color: #b9bbbe">
+      This is a showcase of the finished projects i have~! I am trying to do better about having documentation of my work.
+    </h4>
+  </div>
 </template>
+
+
+<style scoped>
+.container {
+  display: inline;
+  flex-direction: column;
+  height: 100%;
+}
+
+.nav {
+  background-color: #272727;
+  padding: 10px;
+  margin-bottom: 20px;
+  max-width: 500px; /* added */
+  margin: 0 auto; /* added */
+}
+
+.nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.nav li {
+  display: inline-block;
+  margin-right: 15px;
+}
+
+.nav-item {
+  color: rgb(165, 139, 139);
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 18px;
+  padding: 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.nav-item:hover {
+  background-color: #ddd;
+}
+
+.content {
+  flex: 1;
+}
+</style>
