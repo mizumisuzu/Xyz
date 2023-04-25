e7289e0aff550ff1fb8f9d4c5902968e3d49cb02
_水_ <lightcity123@gmail.com>
2023-04-24 22:02:40 -0500

    rev 0.002

--- src/router.js
+++ src/router.js
@@ -1,17 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/HelloWorld.vue';
-import Projects from './components/projects.vue';
-import Blog from './components/blog.vue';
-import Contact from './components/contact.vue';
+import { createRouter, createWebHistory } from "vue-router";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("./components/HelloWorld.vue")
+  },
+  {
+    path: "/projects",
+    name: "projects",
+    component: () => import("./components/projects.vue")
+  },
+  {
+    path: "/blog",
+    name: "blog",
+    component: () => import("./components/blog.vue")
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: () => import("./components/contact.vue")
+  }
+];
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', component: Home },
-    { path: '/projects', component: Projects },
-    { path: '/blog', component: Blog },
-    { path: '/contact', component: Contact }
-  ]
+  routes
 });
 
 export default router;
