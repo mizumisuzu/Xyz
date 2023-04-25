4b04d0b0ff5b1588d03af42f932caeec13049099
_水_ <lightcity123@gmail.com>
2023-04-24 22:03:01 -0500

    rev 0.002

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import RouterLink from "vue-router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(RouterLink).mount("#app");
